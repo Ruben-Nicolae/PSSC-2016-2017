b45545d3ce401110e93bb7f63443b28f9279f2be
Ruben-Nicolae <ruben.hategan@yahoo.com>
2016-11-06 22:43:21 +0200

    tema2
    factory

---
+++ PSSC/Models/Factory/SubjectsFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Factory
+{
+    class SubjectsFactory
+    {
+        public static readonly SubjectsFactory Instance = new SubjectsFactory();
+        List<Subject.Subject> _subjects;
+
+
+        private SubjectsFactory()
+        {
+
+        }
+
+        public Subject.Subject createInstance(Subject.Subject subject)
+        {
+
+            subject = new Subject.Subject(new Subject.SubjectInformation());
+
+            return subject;
+        }
+    }
+}
